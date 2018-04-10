a2849ca5fe1a5af8a3f4cf0047de14f39e34f679
Stéfano Girardelli <girardellisb@gmail.com>
2018-04-10 18:09:26 -0300

    Creating first working configurations

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ListTheColors
-Get all of the colors of your JSON list based on a google search and the most relevant colors.
+Get all of the colors of your JSON list based on a google image search and their most relevant colors.
+
+Installation:
+------------
+
+Based on: https://github.com/abenassi/Google-Search-API and https://market.mashape.com/apicloud/colortag#
+
+First install Unirest package from pip:
+
+```
+$ pip install unirest
+$ pip install selenium==2.43.0
+pip install colour
+```
+
+Install Firefox 32.0.3 to work with Selenium 2.43.0:
+https://ftp.mozilla.org/pub/firefox/releases/32.0.3
+
+Then install Google Search API:
+
+```
+pip install Google-Search-API
+```
+
+To upgrade the package if you have already installed it:
+
+```
+pip install Google-Search-API --upgrade
+```
---
+++ listthecolors.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from google import google, images
+from bs4 import BeautifulSoup
+from pprint import pprint
+from collections import Counter
+from colour import Color
+import unirest
+import json
+import csv
+
+colorsToSave = []
+
+
+def getColors(url, sort='weight', pallete='w3c') :
+  return unirest.get("https://apicloud-colortag.p.mashape.com/tag-url.json?palette="+pallete+"&sort="+sort+"&url="+url,
+    headers={
+      "X-Mashape-Key": "zkf3ElhK0imshKx1IrjCpiIgjBhGp1GM8rGjsnYbqUem7tO460",
+      "Accept": "application/json"
+    }
+  ).body
+
+
+def getRelevantColor(colorJson):
+  aux = [0, 0]
+  for i in colorJson:
+    if (colorJson[i] > aux[1]):
+      aux[0] = i
+      aux[1] = colorJson[i]
+  return aux[0]
+
+
+def findTheColor(strToFind, precision = 10):
+  print "\nLooking for %s" % (strToFind)
+  options = images.ImageOptions()
+  results = google.search_images(strToFind + " logo", options, precision)
+
+  colorHexa = []
+  colorLabel = []
+
+  for i in range(0, precision):
+    colors = getColors(results[i].link)
+    if colors:
+      for z in range(0, len(colors['tags'])):
+        if (colors['tags'][z]['label'] not in ['Beige', 'White', 'Black']):
+          colorHexa.append(colors['tags'][z]['color'])
+          colorLabel.append(colors['tags'][z]['label'])
+          break
+
+  obj = Counter(colorLabel)
+  c = Color(getRelevantColor(obj))
+  colorsToSave.append(c.hex)
+  print "Color is: %s - %s" % (c, c.hex)
+
+
+# with open('bancos.csv', 'rb') as f:
+#   reader = csv.reader(f)
+#   for row in reader:
+#     findTheColor(row[2], 20)
+#     sleep(30)
+
+precision = 15
+options = images.ImageOptions()
+results = google.search_images("Woori Bank Logo", options, precision)
+
+colorHexa = []
+colorLabel = []
+
+for i in range(0, precision):
+  colors = getColors(results[i].link)
+  print "(%s / %s) %s" % (i+1, precision, results[i].link)
+  if colors:
+    for z in range(0, len(colors['tags'])):
+      if (colors['tags'][z]['label'] not in ['Beige', 'White', 'Black', 'WhiteSmoke', 'gainsboro']):
+        # print "-> " + colors['tags'][z]['label'] + " - " + colors['tags'][z]['color']
+        colorHexa.append(colors['tags'][z]['color'])
+        colorLabel.append(colors['tags'][z]['label'])
+        break
+
+print ""
+obj = Counter(colorLabel)
+c = Color(getRelevantColor(obj))
+print "Color is: %s - %s" % (c, c.hex)
+# print "To use on Equals: %s" % (Color(c, luminance=0.2).hex) 
